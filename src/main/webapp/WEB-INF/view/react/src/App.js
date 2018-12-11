f2e5b516db534d0aaf4c7a0d792b9c4395daa225
hgroome97 <harrison.groome@outlook.com>
2018-12-11 09:40:41 +0000

    ready for routing

--- src/main/webapp/WEB-INF/view/react/src/App.js
+++ src/main/webapp/WEB-INF/view/react/src/App.js
@@ -1,34 +1,14 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import Form from './Form'
-
-const inputs = [{
-  name: "username",
-  placeholder: "username",
-  type: "text"
-},{
-  name: "password",
-  placeholder: "password",
-  type: "password"
-},{
-  type: "submit",
-  value: "Submit",
-  className: "btn"
-}]
-
-const props = {name: 'loginForm', method: 'POST', action: '/perform_login', inputs: inputs}
-
-const params = new URLSearchParams(window.location.search)
+import Login from './Login.js'
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <Form {...props} error={params.get('error')} />
+        <Login />
       </div>
     );
   }
 }
-
-//ReactDOM.render(<Form {...props} error={params.get('error')} />, document.getElementById('container'))
